8841b24049e2447d2f28903d713f107eaa8ad5e2
Bercin <bercintatar@gmail.com>
2022-10-15 00:45:17 +0300

    Commit 4

--- assets/js/utils/functions.js
+++ assets/js/utils/functions.js
@@ -4,6 +4,9 @@ const getCharacters = async () =>
 const getLocations = async () =>
   (await (await fetch(`${API_ENDPOINT}/location`)).json()).results;
 
+const getEpisodes = async () =>
+  (await (await fetch(`${API_ENDPOINT}/episode`)).json()).results;
+
 const writePrefetchedList = (selector, stringData) => {
   const element = document.querySelector(selector);
   element.innerHTML = stringData;
